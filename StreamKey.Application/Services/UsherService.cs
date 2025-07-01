1807f06840a523ee092527f04143d4bc10da0f02
Eridani 137 <tsuuzetsu@gmail.com>
2025-07-01 15:42:33 +0300

    [✨ feat] UsherService: Add method to remove ads from HLS playlist using regex patterns
    Changes:
    - Added `RemoveAdsFromPlaylist` method
    - Added regex patterns to detect and remove various ad formats
    - Marked class as `partial` for future extensions

--- StreamKey.Application/Services/UsherService.cs
+++ StreamKey.Application/Services/UsherService.cs
@@ -1,5 +1,5 @@
+using System.Text.RegularExpressions;
 using M3U8Parser;
-using M3U8Parser.Tags.MultivariantPlaylist;
 using StreamKey.Application.DTOs;
 using StreamKey.Application.DTOs.TwitchGraphQL;
 using StreamKey.Application.Results;
@@ -11,17 +11,24 @@ public interface IUsherService
     Task<Result<StreamResponseDto>> Get1080PStream(string username, PlaybackAccessTokenResponse accessToken);
 }
 
-public class UsherService(HttpClient client) : IUsherService
+public partial class UsherService(HttpClient client) : IUsherService
 {
     public static readonly Uri UsherUrl = new("https://usher.ttvnw.net");
-
-    public async Task<Result<StreamResponseDto>> Get1080PStream(string username,
-        PlaybackAccessTokenResponse accessToken)
+    private static readonly Regex[] AdPatterns =
+    [
+        TwitchStitchedAdRegex(),
+        StitchedAdRegex(),
+        TvTwitchAdRegex(),
+        AmazonAdRegex()
+    ];
+    
+    public async Task<Result<StreamResponseDto>> Get1080PStream(string username, PlaybackAccessTokenResponse accessToken)
     {
         var url =
             $"api/channel/hls/{username}.m3u8?client_id={TwitchService.ClientId}&token={accessToken.Data!.StreamPlaybackAccessToken!.Value}&sig={accessToken.Data.StreamPlaybackAccessToken.Signature}&allow_source=true";
         var response = await client.GetStringAsync(url);
-
+        response = RemoveAdsFromPlaylist(response);
+        
         var playlist = MasterPlaylist.LoadFromText(response);
         if (playlist is null) return Result.Failure<StreamResponseDto>(Error.PlaylistNotReceived);
 
@@ -35,4 +42,21 @@ public class UsherService(HttpClient client) : IUsherService
             Source = stream.Uri
         });
     }
+
+    private static string RemoveAdsFromPlaylist(string playlistContent)
+    {
+        return AdPatterns.Aggregate(playlistContent, (current, pattern) => pattern.Replace(current, string.Empty));
+    }
+
+    [GeneratedRegex(@"#EXT-X-DATERANGE:.*twitch-stitched-ad.*\n", RegexOptions.Compiled)]
+    private static partial Regex TwitchStitchedAdRegex();
+
+    [GeneratedRegex(@"#EXT-X-DATERANGE:.*stitched-ad-.*\n", RegexOptions.Compiled)]
+    private static partial Regex StitchedAdRegex();
+
+    [GeneratedRegex(@"#EXT-X-DATERANGE:.*X-TV-TWITCH-AD-.*\n", RegexOptions.Compiled)]
+    private static partial Regex TvTwitchAdRegex();
+
+    [GeneratedRegex(@"#EXTINF:.*Amazon.*\n.*\n", RegexOptions.Compiled)]
+    private static partial Regex AmazonAdRegex();
 }
