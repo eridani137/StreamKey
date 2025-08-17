48237084616efc673f83a60d01e6778cb134554c
Eridani 137 <tsuuzetsu@gmail.com>
2025-08-17 23:44:35 +0300

    ✨ feat(ChannelInfoUpdater.cs): add update timestamp to channel info

--- StreamKey.Core/Services/ChannelInfoUpdater.cs
+++ StreamKey.Core/Services/ChannelInfoUpdater.cs
@@ -41,6 +41,7 @@ public class ChannelInfoUpdater(
                     if (fresh is null) continue;
                     
                     fresh.Info = info;
+                    fresh.Info.UpdateTime = DateTime.Now;
                     await channelRepository.Update(fresh);
                     logger.LogInformation("Канал {ChannelName} обновлен: {@ChannelInfo}", fresh.Name, info);
                 }
