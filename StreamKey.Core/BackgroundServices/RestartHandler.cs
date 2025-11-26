47f836e0614b4a163223e3c6e05469358c3ae0ca
Eridani 137 <tsuuzetsu@gmail.com>
2025-11-26 10:58:26 +0300

    ✨ feat(RestartHandler): Implement scoped services for restart logic

--- StreamKey.Core/BackgroundServices/RestartHandler.cs
+++ StreamKey.Core/BackgroundServices/RestartHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using StreamKey.Infrastructure.Abstractions;
@@ -6,42 +7,46 @@ using StreamKey.Shared.Entities;
 namespace StreamKey.Core.BackgroundServices;
 
 public class RestartHandler(
-    IRestartRepository repository,
-    IUnitOfWork unitOfWork,
+    IServiceProvider serviceProvider,
     IHostApplicationLifetime appLifetime,
     ILogger<RestartHandler> logger)
     : BackgroundService
 {
     private readonly TimeSpan _time = new(1, 0, 0);
     private readonly TimeSpan _checkDelay = TimeSpan.FromMinutes(1);
-    private RestartEntity? _lastRestartEntity;
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _lastRestartEntity = await repository.GetLastRestart();
-            
-            var now = DateTime.UtcNow;
-            var timeToday = new DateTime(now.Year, now.Month, now.Day, _time.Hours, _time.Minutes, _time.Seconds);
-            
-            if (now >= timeToday)
+            using (var scope = serviceProvider.CreateScope())
             {
-                if (_lastRestartEntity is null || _lastRestartEntity.DateTime.Date < now.Date)
-                {
-                    logger.LogInformation("Плановый перезапуск");
+                var repository = scope.ServiceProvider.GetRequiredService<IRestartRepository>();
+                var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
 
-                    await repository.Add(new RestartEntity()
+                var lastRestartEntity = await repository.GetLastRestart();
+
+                var now = DateTime.UtcNow;
+                var timeToday = new DateTime(now.Year, now.Month, now.Day, _time.Hours, _time.Minutes, _time.Seconds);
+
+                if (now >= timeToday)
+                {
+                    if (lastRestartEntity == null || lastRestartEntity.DateTime.Date < now.Date)
                     {
-                        DateTime = now
-                    });
-                    await unitOfWork.SaveChangesAsync(stoppingToken);
-                    
-                    appLifetime.StopApplication();
-                    break;
+                        logger.LogInformation("Плановый перезапуск сервиса");
+
+                        await repository.Add(new RestartEntity
+                        {
+                            DateTime = now
+                        });
+                        await unitOfWork.SaveChangesAsync(stoppingToken);
+
+                        appLifetime.StopApplication();
+                        break;
+                    }
                 }
             }
-            
+
             try
             {
                 await Task.Delay(_checkDelay, stoppingToken);
