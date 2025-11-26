3656282dfe45e76eaa75e992fa525ee32c31a1a7
Eridani 137 <tsuuzetsu@gmail.com>
2025-11-26 11:03:21 +0300

    🐛 fix(RestartRepository): Correctly retrieve last restart for the current day

--- StreamKey.Infrastructure/Repositories/RestartRepository.cs
+++ StreamKey.Infrastructure/Repositories/RestartRepository.cs
@@ -8,8 +8,11 @@ public class RestartRepository(ApplicationDbContext context) : BaseRepository<Re
 {
     public async Task<RestartEntity?> GetLastRestart()
     {
+        var today = DateTime.UtcNow.Date;
+        var tomorrow = today.AddDays(1);
+
         return await GetSet()
-            .Where(e => e.DateTime <= DateTime.UtcNow)
+            .Where(e => e.DateTime >= today && e.DateTime < tomorrow)
             .OrderByDescending(e => e.DateTime)
             .FirstOrDefaultAsync();
     }
