a97a75bd91b1734c0196ef9f6bbf7d2fadd86e32
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-02 22:50:12 +0300

    ✨ feat(QueryExtensions): simplify query authentication and device ID headers

--- StreamKey.Core/Extensions/QueryExtensions.cs
+++ StreamKey.Core/Extensions/QueryExtensions.cs
@@ -10,18 +10,22 @@ public static class QueryExtensions
     {
         public void AddQueryAuth(HttpClient client)
         {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                "OAuth",
-                query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
-                    ? auth
-                    : ApplicationConstants.DefaultAuthorization
-            );
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", ApplicationConstants.DefaultAuthorization);
+            
+            // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            //     "OAuth",
+            //     query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
+            //         ? auth
+            //         : ApplicationConstants.DefaultAuthorization
+            // );
+            
+            client.DefaultRequestHeaders.Add("x-device-id", ApplicationConstants.DefaultDeviceId);
 
-            client.DefaultRequestHeaders.Add("x-device-id",
-                query.TryGetValue("device-id", out var deviceId) && !string.IsNullOrEmpty(deviceId)
-                    ? deviceId
-                    : ApplicationConstants.DefaultDeviceId
-            );
+            // client.DefaultRequestHeaders.Add("x-device-id",
+            //     query.TryGetValue("device-id", out var deviceId) && !string.IsNullOrEmpty(deviceId)
+            //         ? deviceId
+            //         : ApplicationConstants.DefaultDeviceId
+            // );
         }
     }
 }
