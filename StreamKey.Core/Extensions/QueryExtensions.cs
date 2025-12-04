f1807b30274c58a393c3b967792ad9125769cfaf
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-05 00:17:51 +0300

    ✨ feat(QueryExtensions): Implement query parameter-based authorization and device ID headers

--- StreamKey.Core/Extensions/QueryExtensions.cs
+++ StreamKey.Core/Extensions/QueryExtensions.cs
@@ -9,20 +9,17 @@ public static class QueryExtensions
     {
         public void AddQueryAuth(HttpRequestMessage request)
         {
-            // var authorization = query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
-            //     ? auth.ToString()
-            //     : ApplicationConstants.DefaultAuthorization;
-            //
-            // request.Headers.Add("Authorization", authorization);
-            //
-            // var deviceId = query.TryGetValue("device-id", out var device) && !string.IsNullOrEmpty(device)
-            //     ? device.ToString()
-            //     : ApplicationConstants.DefaultDeviceId;
-            //
-            // request.Headers.Add("device-id", deviceId);
+            var authorization = query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
+                ? auth.ToString()
+                : ApplicationConstants.DefaultAuthorization;
             
-            request.Headers.Add("Authorization", ApplicationConstants.DefaultAuthorization);
-            request.Headers.Add("device-id", ApplicationConstants.DefaultDeviceId);
+            request.Headers.Add("Authorization", authorization);
+            
+            var deviceId = query.TryGetValue("device-id", out var device) && !string.IsNullOrEmpty(device)
+                ? device.ToString()
+                : ApplicationConstants.DefaultDeviceId;
+            
+            request.Headers.Add("device-id", deviceId);
         }
 
         public void AddQueryAuth(HttpClient client)
