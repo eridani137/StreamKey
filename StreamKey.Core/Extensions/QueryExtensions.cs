a32c8e54782d60fbce0053db2457780bd1564310
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-05 17:23:06 +0300

    ✨ feat(QueryExtensions): simplify AddQueryAuth to use default authorization

--- StreamKey.Core/Extensions/QueryExtensions.cs
+++ StreamKey.Core/Extensions/QueryExtensions.cs
@@ -9,16 +9,18 @@ public static class QueryExtensions
     {
         public void AddQueryAuth(HttpRequestMessage request)
         {
-            var authorization = query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
-                ? auth.ToString()
-                : ApplicationConstants.DefaultAuthorization;
-            
+            // var authorization = query.TryGetValue("auth", out var auth) && !string.IsNullOrEmpty(auth)
+            //     ? auth.ToString()
+            //     : ApplicationConstants.DefaultAuthorization;
+
+            var authorization = ApplicationConstants.DefaultAuthorization;
+
             request.Headers.Add("Authorization", authorization);
 
             var deviceId = authorization == ApplicationConstants.DefaultAuthorization
                 ? ApplicationConstants.DefaultDeviceId
                 : TwitchExtensions.GenerateDeviceId();
-            
+
             request.Headers.Add("x-device-id", deviceId);
         }
     }
