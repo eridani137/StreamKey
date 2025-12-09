58de57ffb549beface5a64dedacc75a0017bc8fb
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-09 03:38:50 +0300

    ✨ feat(StreamKey.Hub): Configure SignalR to use Redis with dynamic host and password

--- StreamKey.Hub/Program.cs
+++ StreamKey.Hub/Program.cs
@@ -1,14 +1,24 @@
 using StackExchange.Redis;
 using StreamKey.Hub.Hubs;
+using StreamKey.Shared.Configs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR()
-    .AddMessagePackProtocol()
-    .AddStackExchangeRedis("redis:6379", options =>
-    {
-        options.Configuration.ChannelPrefix = RedisChannel.Literal("StreamKey");
-    });
+if (builder.Configuration.GetSection(nameof(RedisConfig)).Get<RedisConfig>() is { } redisConfig &&
+    builder.Configuration.GetSection("RedisHost").Get<string>() is { } redisHost)
+{
+    builder.Services.AddSignalR()
+        .AddMessagePackProtocol()
+        .AddStackExchangeRedis(options =>
+        {
+            options.Configuration = new ConfigurationOptions
+            {
+                EndPoints = { $"{redisHost}:{redisConfig.Port}" },
+                Password = redisConfig.Password,
+                ChannelPrefix = RedisChannel.Literal("StreamKey")
+            };
+        });
+}
 
 var app = builder.Build();
 
