f427c0aee69c00d186df14029de29c46ed249030
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-14 07:56:53 +0300

    ♻️ refactor(Activity/Statistic): Rename 'Online' to 'ConnectionsCount' for clarity in DTO and API

--- StreamKey.Shared/DTOs/Activity.cs
+++ StreamKey.Shared/DTOs/Activity.cs
@@ -16,7 +16,7 @@ public record OnlineResponse
 {
     public int TotalOnline { get; init; }
     public int OldVersionsOnline { get; init; }
-    public int Online { get; init; }
+    public int ConnectionsCount { get; init; }
     public int ActiveOnline { get; init; }
     public int SleepingOnline { get; init; } 
 }
