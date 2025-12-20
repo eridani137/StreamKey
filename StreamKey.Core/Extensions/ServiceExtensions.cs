0879a2abc16868d84c587ea8a3cf148f10f2dcaf
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-20 19:55:00 +0300

    🔥 docs(ServiceExtensions): Temporarily disable Restart hosted service

--- StreamKey.Core/Extensions/ServiceExtensions.cs
+++ StreamKey.Core/Extensions/ServiceExtensions.cs
@@ -50,7 +50,7 @@ public static class ServiceExtensions
 
             services.AddHostedService<ChannelsBackground>();
             services.AddHostedService<Statistic>();
-            services.AddHostedService<Restart>();
+            // services.AddHostedService<Restart>();
             services.AddHostedService<BackgroundServices.Telegram>();
 
             services.AddHostedService<ConnectionListener>();
