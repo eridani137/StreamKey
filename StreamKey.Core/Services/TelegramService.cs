8bdb50759a61f4017150235586846705724ae5ca
Eridani 137 <tsuuzetsu@gmail.com>
2025-12-22 21:36:28 +0300

    🐛 fix(TelegramService): Log GetChatMember errors as information

--- StreamKey.Core/Services/TelegramService.cs
+++ StreamKey.Core/Services/TelegramService.cs
@@ -28,7 +28,7 @@ public class TelegramService(ITelegramBotClient botClient, ILogger<TelegramServi
         }
         catch (Exception e)
         {
-            logger.LogError(e, "GetChatMember [{UserId}]", userId);
+            logger.LogInformation(e, "GetChatMember [{UserId}]", userId);
             return null;
         }
     }
